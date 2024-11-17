d745868d9ce792e42fb970198421048147a9b8a7
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-11-17 15:39:09 +0900

    Feat: Link를 이용한 라우팅 추가

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
-import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
-  return <h1 className={styles.title}>안녕 Next.js!</h1>;
+  return (
+    <div>
+      <h1>mall</h1>
+      <ul>
+        <li>
+          <Link href="/products/1">첫 번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/2">두 번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/3">세 번째 상품</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
