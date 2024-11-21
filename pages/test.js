e3393a7dd89e355040b5195ad507a5da41eccbbe
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-11-21 16:22:08 +0900

    Feat: Image컴포넌트의 활용과 외부 이미지 사용하기

---
+++ pages/test.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function Test() {
+  return (
+    <>
+      <div style={{ position: 'relative', width: '50%', height: '200px' }}>
+        <Image
+          fill
+          src="/images/product.jpeg"
+          alt="상품 이미지"
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+    </>
+  );
+}
